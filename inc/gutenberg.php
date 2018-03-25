e852c13e745c642fbfd270ea0921527408708c78
Andrea Barghigiani <andrea@codeat.it>
2018-03-25 11:29:32 +0200

    added wide images support

--- inc/gutenberg.php
+++ inc/gutenberg.php
@@ -5,3 +5,11 @@
  *
  * @package Gutenberg_Theme
  */
+
+/**
+ * Add support for wide images
+ */
+add_action('after_setup_theme', 'add_gutenberg_support');
+function add_gutenberg_support(){
+    add_theme_support('align-wide');
+}
