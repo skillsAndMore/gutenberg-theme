7078b0626e78a32a474d43d1f3f8f726171a5f5d
Andrea Barghigiani <andrea@codeat.it>
2018-04-03 10:26:01 +0200

    fix namespace

--- inc/gutenberg.php
+++ inc/gutenberg.php
@@ -9,8 +9,8 @@
 /**
  * Add support for wide images
  */
-add_action('after_setup_theme', 'add_gutenberg_support');
-function add_gutenberg_support(){
+add_action('after_setup_theme', 'wctrn_add_gutenberg_support');
+function wctrn_add_gutenberg_support(){
     add_theme_support('align-wide');
 
     add_theme_support( 'editor-color-palette',
@@ -21,28 +21,3 @@ function add_gutenberg_support(){
     );
 }
 
-/**
- * Registro CPT con Custom Template con layout a colonne
- */
-// function myplugin_register_book_post_type() {
-// 	$args = array(
-// 		'public' => true,
-// 		'label'  => 'Books',
-// 		'show_in_rest' => true,
-//         'template' => array(
-//             array('core/columns', array(), array(
-//                 array('core/image', array('layout' => 'column-1')),
-//                 array('core/heading', array(
-//                     'placeholder' => 'Add Author...',
-//                     'layout' => 'column-2'
-//                 )),
-//                 array('core/paragraph', array(
-//                     'placeholder' => 'Add a inner paragraph',
-//                     'layout' => 'column-2'
-//                 )),
-//             ))
-//         ),
-// 	);
-// 	register_post_type( 'book', $args );
-// }
-// add_action( 'init', 'myplugin_register_book_post_type' );
